62ad149509429791955364fcd89f125dfedf336d
Josh King <jking@chambana.net>
2018-10-11 17:48:17 -0400

    Integrate eslint, flow, prettier, husky, babel, and unit testing tools.

--- src/signal.js
+++ src/signal.js
@@ -4,37 +4,52 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-'use strict';
+"use strict";
 
-const ByteBuffer = require('bytebuffer');
-const Api = require('libsignal-service');
+const ByteBuffer = require("bytebuffer");
+const Api = require("libsignal-service");
 //const ProtocolStore = require('./protocol_store.js');
-const ProtocolStore = require('./LocalSignalProtocolStore.js');
-const Adapter = require('hubot/es2015').Adapter;
-const TextMessage = require('hubot/es2015').TextMessage;
-const User = require('hubot/es2015').User;
+const ProtocolStore = require("./LocalSignalProtocolStore.js");
+const Adapter = require("hubot/es2015").Adapter;
+const TextMessage = require("hubot/es2015").TextMessage;
+const User = require("hubot/es2015").User;
 
+// @flow
 class Signal extends Adapter {
-
-  constructor(...args) {
-    super(...args);
+  // @flow
+  constructor(args?: Array<mixed>): void {
+    super(args);
     this.number = process.env.HUBOT_SIGNAL_NUMBER;
     this.password = process.env.HUBOT_SIGNAL_PASSWORD;
     //this.store = new ProtocolStore(this.robot.brain);
-    this.store = new ProtocolStore('./scratch');
-    this.accountManager = new Api.AccountManager(this.number, this.password, this.store);
+    this.store = new ProtocolStore("./scratch");
+    this.accountManager = new Api.AccountManager(
+      this.number,
+      this.password,
+      this.store
+    );
     this.robot.logger.info("Constructed!");
   }
 
-  send(envelope, ...strings) {
-    if ((envelope.room == null)) {
-      this.robot.logger.error("Cannot send a message without a valid room. Envelopes should contain a room property set to a phone number.");
+  // @flow
+  send(envelope, strings: Array<string>) {
+    if (envelope.room == null) {
+      this.robot.logger.error(
+        "Cannot send a message without a valid room. Envelopes should contain a room property set to a phone number."
+      );
       return;
     }
     const text = strings.join();
     const now = Date.now();
     this.messageSender
-      .sendMessageToNumber(envelope.room, text, null, now, undefined, this.store.get('profileKey'))
+      .sendMessageToNumber(
+        envelope.room,
+        text,
+        null,
+        now,
+        undefined,
+        this.store.get("profileKey")
+      )
       .then(function(result) {
         return this.robot.logger.info("result");
       })
@@ -42,15 +57,25 @@ class Signal extends Adapter {
     this.robot.logger.info("Send");
   }
 
+  // @flow
   reply(envelope, ...strings) {
-    if ((envelope.room == null)) {
-      this.robot.logger.error("Cannot send a message without a valid room. Envelopes should contain a room property set to a phone number.");
+    if (envelope.room == null) {
+      this.robot.logger.error(
+        "Cannot send a message without a valid room. Envelopes should contain a room property set to a phone number."
+      );
       return;
     }
     const text = strings.join();
     const now = Date.now();
     this.messageSender
-      .sendMessageToNumber(envelope.room, text, null, now, undefined, this.store.get('profileKey'))
+      .sendMessageToNumber(
+        envelope.room,
+        text,
+        null,
+        now,
+        undefined,
+        this.store.get("profileKey")
+      )
       .then(function(result) {
         return this.robot.logger.info(result);
       })
@@ -58,23 +83,27 @@ class Signal extends Adapter {
     this.robot.logger.info("Reply");
   }
 
+  // @flow
   _request() {
     this.robot.logger.info("Requesting code.");
-    return this.accountManager
-      .requestSMSVerification(this.number);
+    return this.accountManager.requestSMSVerification(this.number);
     //  .catch(this.robot.logger.error);
   }
 
+  // @flow
   _register() {
     this.robot.logger.info("Registering account.");
-    return this.accountManager
-      .registerSingleDevice(this.number, process.env.HUBOT_SIGNAL_CODE);
-      //.then(function(result) {
-      //  this.robot.logger.info(result);
-      //})
-      //.catch(this.robot.logger.error);
+    return this.accountManager.registerSingleDevice(
+      this.number,
+      process.env.HUBOT_SIGNAL_CODE
+    );
+    //.then(function(result) {
+    //  this.robot.logger.info(result);
+    //})
+    //.catch(this.robot.logger.error);
   }
 
+  // @flow
   _receive(userId, text, msgId, group) {
     if (group == null) {
       group = userId;
@@ -88,46 +117,78 @@ class Signal extends Adapter {
     this.robot.receive(message);
   }
 
+  // @flow
   _connect() {
     this.robot.logger.info("Connecting to service.");
     const signalingKey = this.store.get("signaling_key");
     if (!signalingKey) {
-      this.robot.logger.error("No signaling key is defined, perhaps we didn't successfully register?");
+      this.robot.logger.error(
+        "No signaling key is defined, perhaps we didn't successfully register?"
+      );
       process.exit(1);
     }
 
-    this.messageSender = new Api.MessageSender(this.number, this.password, this.store);
+    this.messageSender = new Api.MessageSender(
+      this.number,
+      this.password,
+      this.store
+    );
     const signalingKeyBytes = ByteBuffer.wrap(
       signalingKey,
       "binary"
     ).toArrayBuffer();
-    this.messageReceiver = new Api.MessageReceiver(this.number.concat(".1"), this.password, signalingKeyBytes, this.store);
+    this.messageReceiver = new Api.MessageReceiver(
+      this.number.concat(".1"),
+      this.password,
+      signalingKeyBytes,
+      this.store
+    );
     this.messageReceiver.addEventListener("message", ev => {
       const id = ev.data.source.toString();
-      this._receive(id, ev.data.message.body.toString(), ev.data.timestamp.toString(), ev.data.message.group);
+      this._receive(
+        id,
+        ev.data.message.body.toString(),
+        ev.data.timestamp.toString(),
+        ev.data.message.group
+      );
     });
     this.emit("connected");
   }
 
+  // @flow
   run() {
     this.robot.logger.info("Running adapter.");
-    if (!this.store.get('profileKey')) {
+    if (!this.store.get("profileKey")) {
       if (!process.env.HUBOT_SIGNAL_CODE) {
-        Promise.resolve(this._request()).then(result => {
-          this.robot.logger.info(`Sending verification code to ${this.number}. Once you receive the code, start the bot again while supplying the code via the environment variable HUBOT_SIGNAL_CODE.`);
-          process.exit(0);
-        }).catch(err => {
-          this.robot.logger.error('Error requesting verification code: ', err.stack);
-          process.exit(1);
-        });
+        Promise.resolve(this._request())
+          .then(result => {
+            this.robot.logger.info(
+              `Sending verification code to ${
+                this.number
+              }. Once you receive the code, start the bot again while supplying the code via the environment variable HUBOT_SIGNAL_CODE.`
+            );
+            process.exit(0);
+          })
+          .catch(err => {
+            this.robot.logger.error(
+              "Error requesting verification code: ",
+              err.stack
+            );
+            process.exit(1);
+          });
       } else {
-        Promise.resolve(this._register()).then(result => {
-          this.robot.logger.info(result);
-          this._connect();
-        }).catch(err => {
-          this.robot.logger.error('Error registering with service: ', err.stack);
-          process.exit(1);
-        })
+        Promise.resolve(this._register())
+          .then(result => {
+            this.robot.logger.info(result);
+            this._connect();
+          })
+          .catch(err => {
+            this.robot.logger.error(
+              "Error registering with service: ",
+              err.stack
+            );
+            process.exit(1);
+          });
       }
     } else {
       this._connect();
@@ -135,4 +196,4 @@ class Signal extends Adapter {
   }
 }
 
-exports.use = robot => new Signal(robot)
+exports.use = robot => new Signal(robot);
