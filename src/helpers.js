6d88c8c92475e6df0015bf822e788d34db89ea17
Josh King <jking@chambana.net>
2019-06-30 11:26:06 -0400

    Update protocol store and helpers to reflect latest libsignal-service.

--- src/helpers.js
+++ src/helpers.js
@@ -1,25 +1,27 @@
 /*
- * vim: ts=4:sw=4:expandtab
+ * vim: ts=2:sw=2:expandtab
  */
 
 var ByteBuffer = require("bytebuffer");
+/* eslint-disable no-proto, no-restricted-syntax, guard-for-in */
 
-/*********************************
+/** *******************************
  *** Type conversion utilities ***
- *********************************/
+ ******************************** */
 // Strings/arrays
-//TODO: Throw all this shit in favor of consistent types
-//TODO: Namespace
-var StaticByteBufferProto = new ByteBuffer().__proto__;
-var StaticArrayBufferProto = new ArrayBuffer().__proto__;
-var StaticUint8ArrayProto = new Uint8Array().__proto__;
+// TODO: Throw all this shit in favor of consistent types
+// TODO: Namespace
+const StaticByteBufferProto = new ByteBuffer().__proto__;
+const StaticArrayBufferProto = new ArrayBuffer().__proto__;
+const StaticUint8ArrayProto = new Uint8Array().__proto__;
+
 function getString(thing) {
   if (thing === Object(thing)) {
-    if (thing.__proto__ == StaticUint8ArrayProto)
+    if (thing.__proto__ === StaticUint8ArrayProto)
       return String.fromCharCode.apply(null, thing);
-    if (thing.__proto__ == StaticArrayBufferProto)
+    if (thing.__proto__ === StaticArrayBufferProto)
       return getString(new Uint8Array(thing));
-    if (thing.__proto__ == StaticByteBufferProto)
+    if (thing.__proto__ === StaticByteBufferProto)
       return thing.toString("binary");
   }
   return thing;
@@ -27,73 +29,66 @@ function getString(thing) {
 
 function getStringable(thing) {
   return (
-    typeof thing == "string" ||
-    typeof thing == "number" ||
-    typeof thing == "boolean" ||
+    typeof thing === "string" ||
+    typeof thing === "number" ||
+    typeof thing === "boolean" ||
     (thing === Object(thing) &&
-      (thing.__proto__ == StaticArrayBufferProto ||
-        thing.__proto__ == StaticUint8ArrayProto ||
-        thing.__proto__ == StaticByteBufferProto))
+      (thing.__proto__ === StaticArrayBufferProto ||
+        thing.__proto__ === StaticUint8ArrayProto ||
+        thing.__proto__ === StaticByteBufferProto))
   );
 }
 
-// Number formatting utils
-exports = module.exports = (function() {
-  var self = {};
-  self.unencodeNumber = function(number) {
-    return number.split(".");
-  };
-
-  self.isNumberSane = function(number) {
-    return number[0] == "+" && /^[0-9]+$/.test(number.substring(1));
-  };
-
-  self.getString = function(thing) {
-    if (thing === Object(thing)) {
-      if (thing.__proto__ == StaticUint8ArrayProto)
-        return String.fromCharCode.apply(null, thing);
-      if (thing.__proto__ == StaticArrayBufferProto)
-        return getString(new Uint8Array(thing));
-      if (thing.__proto__ == StaticByteBufferProto)
-        return thing.toString("binary");
-    }
-    return thing;
-  };
-
-  /**************************
-   *** JSON'ing Utilities ***
-   **************************/
-  function ensureStringed(thing) {
-    if (getStringable(thing)) return getString(thing);
-    else if (thing instanceof Array) {
-      var res = [];
-      for (var i = 0; i < thing.length; i++) res[i] = ensureStringed(thing[i]);
-      return res;
-    } else if (thing === Object(thing)) {
-      var res = {};
-      for (var key in thing) res[key] = ensureStringed(thing[key]);
-      return res;
-    } else if (thing === null) {
-      return null;
-    }
-    throw new Error("unsure of how to jsonify object of type " + typeof thing);
+function stringToArrayBuffer(str) {
+  if (typeof str !== "string") {
+    throw new Error("Passed non-string to stringToArrayBuffer");
   }
+  var res = new ArrayBuffer(str.length);
+  var uint = new Uint8Array(res);
+  for (var i = 0; i < str.length; i++) {
+    uint[i] = str.charCodeAt(i);
+  }
+  return res;
+}
 
-  self.stringToArrayBuffer = function(str) {
-    if (typeof str !== "string") {
-      throw new Error("Passed non-string to stringToArrayBuffer");
-    }
-    var res = new ArrayBuffer(str.length);
-    var uint = new Uint8Array(res);
-    for (var i = 0; i < str.length; i++) {
-      uint[i] = str.charCodeAt(i);
-    }
+// Number formatting utils
+function unencodeNumber(number) {
+  return number.split(".");
+}
+
+function isNumberSane(number) {
+  return number[0] === "+" && /^[0-9]+$/.test(number.substring(1));
+}
+
+/** ************************
+ *** JSON'ing Utilities ***
+ ************************* */
+function ensureStringed(thing) {
+  if (getStringable(thing)) return getString(thing);
+  else if (thing instanceof Array) {
+    const res = [];
+    for (let i = 0; i < thing.length; i += 1) res[i] = ensureStringed(thing[i]);
     return res;
-  };
+  } else if (thing === Object(thing)) {
+    const res = {};
+    for (const key in thing) res[key] = ensureStringed(thing[key]);
+    return res;
+  } else if (thing === null) {
+    return null;
+  }
+  throw new Error(`unsure of how to jsonify object of type ${typeof thing}`);
+}
 
-  self.jsonThing = function(thing) {
-    return JSON.stringify(ensureStringed(thing));
-  };
+function jsonThing(thing) {
+  return JSON.stringify(ensureStringed(thing));
+}
 
-  return self;
-})();
+exports = module.exports = {
+  getString,
+  getStringable,
+  unencodeNumber,
+  isNumberSane,
+  stringToArrayBuffer,
+  ensureStringed,
+  jsonThing
+};
--- src/protocol_store.js
+++ src/protocol_store.js
@@ -185,7 +185,7 @@ SignalProtocolStore.prototype = {
       return Promise.resolve(true);
     }
     return Promise.resolve(
-      helpers.toString(identityKey) === helpers.toString(trusted)
+      helpers.toString(identityKey) === helpers.toString(trusted.publicKey)
     );
   },
   loadIdentityKey: function(identifier) {
@@ -460,21 +460,27 @@ SignalProtocolStore.prototype = {
     return Promise.resolve();
   },
   getUnprocessed: function(id) {
-    var collection = [];
-    for (let id of Object.keys(this.robot.brain.data._private)) {
-      if (id.startsWith("unprocessed" + id)) {
-        collection.push(this.get(id));
+    for (let key of this.store._keys) {
+      if (key == "unprocessed" + id) {
+        return this.get(key);
       }
     }
-    return Promise.resolve(collection);
   },
   addUnprocessed: function(data) {
     return Promise.resolve(this.put("unprocessed" + data.id, data));
   },
-  updateUnprocessed: function(id, updates) {
+  updateUnprocessedWithData: function(id, updates) {
     var unprocessed = this.get("unprocessed" + id, { id: id });
     Object.assign(unprocessed, updates);
     return Promise.resolve(this.put("unprocessed" + id, unprocessed));
+  },
+  updateAttemptsUnprocessed: function(id, attempts) {
+    var unprocessed = this.get("unprocessed" + id, { id: id });
+    unprocessed.attempts = attempts;
+    return Promise.resolve(this.put("unprocessed" + id, unprocessed));
+  },
+  addDecryptedDataUnprocessed: function(id, data) {
+    return Promise.resolve(this.updateUnprocessedWithData(id, data));
   },
   removeUnprocessed: function(id) {
     return Promise.resolve(this.remove("unprocessed" + id));
@@ -501,6 +507,19 @@ SignalProtocolStore.prototype = {
   userGetDeviceName: function() {
     return this.get("device_name");
   },
+
+  userSetDeviceNameEncrypted: function() {
+    return this.put("deviceNameEncrypted", true);
+  },
+
+  userGetDeviceNameEncrypted: function() {
+    return this.get("deviceNameEncrypted");
+  },
+
+  userGetSignalingKey: function() {
+    return this.get("signaling_key");
+  },
+
   // GROUP STORAGE
   groupsCreateNewGroup: function(numbers, groupId) {
     var groupId = groupId;
